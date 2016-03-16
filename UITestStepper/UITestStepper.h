e5980eff00cb346e94bd6b2e8c965db7eb5bbbf9
Marek Mechura <marysmech@gmail.com>
2016-03-16 09:44:56 +0100

    Initial Commit

---
+++ UITestStepper/UITestStepper.h
@@ -0,0 +1,19 @@
+//
+//  UITestStepper.h
+//  UITestStepper
+//
+//  Created by Marek Mechura on 16/03/16.
+//  Copyright © 2016 Marek Mechura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UITestStepper.
+FOUNDATION_EXPORT double UITestStepperVersionNumber;
+
+//! Project version string for UITestStepper.
+FOUNDATION_EXPORT const unsigned char UITestStepperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UITestStepper/PublicHeader.h>
+
+
