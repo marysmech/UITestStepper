d6be228b8b954367fc19023b668716c9399f10c3
Marek Mechura <marysmech@gmail.com>
2016-03-16 09:56:39 +0100

    init comimit with podspec

---
+++ UITestStepper/UITestStepper.swift
@@ -0,0 +1,20 @@
+//
+//  UITestStepper.swift
+//  UITestStepper
+//
+//  Created by Marek Mechura on 16/03/16.
+//  Copyright © 2016 Marek Mechura. All rights reserved.
+//
+
+import Foundation
+
+
+public class UITestStepper {
+    public init (){
+        print("Class has been initialised")
+    }
+    
+    public func doSomething(){
+        print("Yeah, it works")
+    }
+}
